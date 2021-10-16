b1395ada23bd6eb24f3dc4abe841ae22bf865d6d
PaulFfrench <36425049+PaulFfrench@users.noreply.github.com>
2021-10-16 13:56:14 +0100

    V1.0 - Adding Logging Support

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,10 @@
 package org.wit.freedomfood
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>){
+    logger.info { "Launching FreedomFood Console App" }
     println("FreedomFood Kotlin App Version 1.0")
 }
